ad61cc9359e186a6cea5aeb593a885aa71a30ae6
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-09-29 09:05:03 +0200

    Prosecuzione es 04 - Dispatcher

--- Node.js/TypeScript/es 04 - Dispatcher/dispatcher.ts
+++ Node.js/TypeScript/es 04 - Dispatcher/dispatcher.ts
@@ -4,11 +4,12 @@ import * as _url from "url";
 import * as _fs from "fs"; 
 import * as _mime from "mime"; 
 import { addListener } from "process";
+import { inherits } from "util";
 let HEADERS = require("headers.json");
 let paginaErrore : string;
 class Dispatcher{
     /*definisco proprietà e metodi*/
-    prompt:string=">>>";
+    prompt:string=">>>  ";
      //Un listener ha come chiave il nome della risorsa e come valore la callback da eseguire
      // {"nomeRisorsa":"Callback"}
      //I listener sono suddivisi in base al tipo di chiamata
@@ -19,14 +20,16 @@ class Dispatcher{
         "PUT":{},
         "PATCH":{}
     };
-    constructor(){}
+
+    constructor(){
+        init();
+    }
+
     addListener(metodo:string, risorsa:string, callaback:any)
     {
         metodo=metodo.toUpperCase();
-       /* if(this.listeners[metodo]) //Controllo che sia diverso da null
-        {
-            
-        }*/
+       /* if(this.listeners[metodo]) //Controllo che sia diverso da null*/
+
         if(metodo in this.listeners) //Controllo se il metodo esiste
         {
             //Vado nel vettore del metodo corrispondente, creare una nuova chiave di nome risorsa e con la rispettiva callback
@@ -38,4 +41,81 @@ class Dispatcher{
             
         }
     }
+
+    dispatch(req,res)
+    {
+        //lettura di motodo, risorsa e parametri
+        let metodo = req.method;
+        let url = _url.parse(req.url, true);
+        let risorsa = url.pathname;
+        let parametri = url.query;
+
+        console.log(`${this.prompt} ${metodo}:${risorsa}${JSON.stringify(parametri)}`);
+
+        if(risorsa.startsWith("/api/"))
+        {
+            if(risorsa in this.listeners[metodo])
+            {
+                let _callback = this.listeners[metodo][risorsa]; //vado a cercare ciò che devo eseguire con la callback
+                _callback(req,res); //lancio in esecuzione la callback
+            }
+            else
+            {
+                //Errore 404
+                res.writeHead(404,HEADERS.text);
+                res.write("Servizio non trovato");
+                res.end();
+            }
+        }
+        else
+        {
+            //Richiesta di una pagina che vado a cercare dal File System
+            //Per comodità chiamo una procedura esterna
+            staticListener(req,res,risorsa);
+        }
+    }
 }
+
+//Metto nella classe solo le funzioni che poi devo o posso richiamare dal main
+//Le altre vanno dichiarate qui fuori con la sintassi normale
+function staticListener(req,res,risorsa)
+{
+    if(risorsa=="/")
+    {
+        risorsa= "/index.html"; //le risorse iniziano sempre con lo /
+    }
+    let fileName ="./static"+risorsa; //i nomi dei file inizano sempre con il percorso -> ./ = "Parti dalla cartella"
+    _fs.readFile(fileName, function(err,data){
+        if(!err)
+        {
+            res.writeHead(200,_mime.getType(fileName)); //Non so se è un file html o css, ecc
+            res.write(data);
+            res.end();
+        }
+        else
+        {
+            //il client si aspetta una pagina
+            res.writeHead(404,HEADERS.html);
+            res.write(paginaErrore);
+            res.end();
+        }
+    });
+}
+
+function init()
+{
+    _fs.readFile("./static/error.html", function(err,data){
+    if(!err)
+    {
+        paginaErrore = data.toString();
+    }
+    else
+    {
+        paginaErrore="<h1>Pagina non trovata :(</h1>"
+    }
+    });
+}
+
+//Export 
+module.exports = new Dispatcher();
+
---
+++ Node.js/TypeScript/es 04 - Dispatcher/server.ts
@@ -0,0 +1,21 @@
+import * as _http from "http";     //import per le librerie di sistema, require per le nostre librerie
+let HEADERS = require("./headers.json"); //./ cerca nella cartella corrente
+let _dispatcher = require("./dispatcher.ts"); //Importa soltanto ciò che è stato esportato in dispatcher.ts
+let port : number = 1337;
+//Primo step: Creare il server
+let server = _http.createServer(function(req,res){  //Funzione di callback richiamata ad ogni arrivo di una richiesta, con i 2 parametri req e res
+    //Verifico se si tratta di una richiesta di risorsa statica (pagina) o dinamica (servizio)
+    _dispatcher.dispatch(req, res); 
+});
+
+//Metto in ascolto il server sulla porta 
+server.listen(port);
+console.log("Server in ascolto sulla porta "+port);
+
+//Registrazione dei servizi
+_dispatcher.addListener("POST","/api/servizio1", function(req, res){ //Parametri: tipo di richiesta, tipo di risorsa e function anonima di callback con req e ris come parametri
+    //Codice di risposta al servizio
+    res.writeHead(200,HEADERS.json);
+    res.write(JSON.stringify({"ris":"ok"}));
+    res.end();
+}); 
