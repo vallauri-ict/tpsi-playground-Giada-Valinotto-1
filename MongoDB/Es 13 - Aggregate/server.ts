177790f6d66aacf20bd8db064575819bd0a68591
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-11-10 10:08:39 +0100

    Metodo AGGREGATE

--- MongoDB/Es 13 - Aggregate/server.ts
+++ MongoDB/Es 13 - Aggregate/server.ts
@@ -13,15 +13,26 @@ const dispatcher: Dispatcher = new Dispatcher();
 const CONNECTIONSTRING = "mongodb://127.0.0.1:27017";
 const DBNAME = "5B";
 
-//Query di riferimento
-mongoClient.connect(CONNECTIONSTRING, (err, client) => {
+  /************ PROVA DEGLI ESEMPI SULLE DISPENSE *************/
+
+//ESEMPIO 1 
+//Si vogliono selezionare i record che hanno status A, raggrupparli secondo il campo cust_id e, per ogni
+//gruppo, eseguire la somma del campo amount (importo unitario)
+//I nome dei campi devono essere oreceduti da $ SOLO SE SONO USATI A DESTRA DEI :
+//Dopo aver fatto i gruppi con $group, il recordset risultante avrà solo 2 colonne: _id e totale.
+//Gli altri campi non sono più visibili
+ mongoClient.connect(CONNECTIONSTRING, (err, client) => {
     if (!err) {
       let db = client.db(DBNAME)
-      let collection = db.collection('Unicorns')
-      let req = collection.find({ weight: { $lte: 800, $gte: 700 } }).toArray()
+      let collection = db.collection('orders')
+      let req = collection.aggregate([
+          {"$match":{"status":"A"}},
+          {"$group":{"_id":"$cust_id", "totale":{"$sum":"$amount"}}},
+          {"$sort":{"totale":-1}}
+        ]).toArray();
   
       req.then((data) => {
-        console.log('Esempio: ', data)
+        console.log('ESEMPIO 1: ', data)
       })
       req.catch((err) => {
         console.log('Errore esecuzione query: ' + err.message)
@@ -34,7 +45,31 @@ mongoClient.connect(CONNECTIONSTRING, (err, client) => {
       console.log('Errore connessione al db: ' + err.message)
     }
   })
-  /************ PROVA DEGLI ESEMPI SULLE DISPENSE *************/
 
-  //Si vogliono selezionare i record che hanno status A, raggrupparli secondo il campo cust_id e, per ogni
- //gruppo, eseguire la somma del campo amount (importo unitario)
+//ESEMPIO 2
+mongoClient.connect(CONNECTIONSTRING, (err, client) => {
+    if (!err) {
+      let db = client.db(DBNAME)
+      let collection = db.collection('orders')
+      let req = collection.aggregate([
+            {"$group":{
+                "_id":"$cust_id",
+                "media":{"$avg":"$amount"}, //media dei tre record 
+                "mediaProdotto":{"$avg":{"$multiply": ["$qta","$amount"]}} //media del prodotto tra la quantità e l'amount
+            }}
+        ]).toArray();
+  
+      req.then((data) => {
+        console.log('ESEMPIO 2: ', data)
+      })
+      req.catch((err) => {
+        console.log('Errore esecuzione query: ' + err.message)
+      })
+  
+      req.finally(() => {
+        client.close()
+      })
+    } else {
+      console.log('Errore connessione al db: ' + err.message)
+    }
+  })
