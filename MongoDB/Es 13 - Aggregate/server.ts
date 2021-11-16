2293074b4d86d59ef2daf9f9c206949429fb6abe
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-11-16 17:49:33 +0100

    Prosecuzione con gli esempi

--- MongoDB/Es 13 - Aggregate/server.ts
+++ MongoDB/Es 13 - Aggregate/server.ts
@@ -73,3 +73,94 @@ mongoClient.connect(CONNECTIONSTRING, (err, client) => {
       console.log('Errore connessione al db: ' + err.message)
     }
   })
+
+  //ESEMPIO 3
+  //Conteggio degli unicorni maschi e degli unicorni femmina
+mongoClient.connect(CONNECTIONSTRING, (err, client) => {
+  if (!err) {
+    let db = client.db(DBNAME)
+    let collection = db.collection('Unicorns')
+    let req = collection.aggregate([
+      {"$match":{"gender":{"$exists":true}}},
+      //uso match per non visualizzare gli unicorni che non hanno uno specifico gender, ovvero
+      //per i quali non esiste il campo gender
+          {"$group":{
+              "_id":"$gender", //campo sul quale vengono fatti i gruppi
+              "totale":{"$sum":1} //somma uno ogni volta 
+          }}
+      ]).toArray();
+
+    req.then((data) => {
+      console.log('ESEMPIO 3: ', data)
+    })
+    req.catch((err) => {
+      console.log('Errore esecuzione query: ' + err.message)
+    })
+
+    req.finally(() => {
+      client.close()
+    })
+  } else {
+    console.log('Errore connessione al db: ' + err.message)
+  }
+})
+
+//ESEMPIO 4
+// Calcolare il numero medio di vampiri uccisi dagli unicorni femmina e dagli unicorni maschi
+mongoClient.connect(CONNECTIONSTRING, (err, client) => {
+    if (!err) {
+      let db = client.db(DBNAME)
+      let collection = db.collection('orders')
+      let req = collection.aggregate([
+            {
+              "$group":{
+                "_id":{"gender":"$gender"}, //è simile all'utilizzo di un alias 
+                "mediaVampiri":{"$avg":"$vampires"}, //media dei tre record 
+            }}
+        ]).toArray();
+  
+      req.then((data) => {
+        console.log('ESEMPIO 4: ', data)
+      })
+      req.catch((err) => {
+        console.log('Errore esecuzione query: ' + err.message)
+      })
+  
+      req.finally(() => {
+        client.close()
+      })
+    } else {
+      console.log('Errore connessione al db: ' + err.message)
+    }
+  })
+
+  //ESEMPIO 5
+  //Aggregazione su campi multipli
+  //La sintassi dell‟esempio precedente consente di raggruppare i record utilizzando chiavi multiple.
+ //Esempio: raggruppare gli unicorni per genere e tipo di pelo
+mongoClient.connect(CONNECTIONSTRING, (err, client) => {
+  if (!err) {
+    let db = client.db(DBNAME)
+    let collection = db.collection('orders')
+    let req = collection.aggregate([
+          {"$group":{
+              "_id":{"gender":"$gender","hair":"$hair"},
+              "nEsemplari":{"$sum":1}, 
+          }},
+          {"$sort":{"nEsemplari":-1,"id_gender":-1}}
+      ]).toArray();
+
+    req.then((data) => {
+      console.log('ESEMPIO 5: ', data)
+    })
+    req.catch((err) => {
+      console.log('Errore esecuzione query: ' + err.message)
+    })
+
+    req.finally(() => {
+      client.close()
+    })
+  } else {
+    console.log('Errore connessione al db: ' + err.message)
+  }
+})
