6c9ca17521b0cbd9ac371c13158a05719374ac6c
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2022-02-24 08:43:43 +0100

    Starting first Cordova exercise

--- Cordova/es01_crudserver/www/index.js
+++ Cordova/es01_crudserver/www/index.js
@@ -1,160 +1,177 @@
-"use strict";
+"use strict"
 
-$(document).ready(function () {
-  let divIntestazione = $("#divIntestazione");
-  let divCollections = $("#divCollections");
-  let table = $("#mainTable");
-  let divDettagli = $("#divDettagli");
+$(document).ready(function() {
+  let filter = $(".card").first();
+  let divIntestazione = $("#divIntestazione")
+  let divCollections = $("#divCollections")
+  let table = $("#mainTable")
+  let divDettagli = $("#divDettagli")
   let currentCollection = "";
-  let filters = $(".card").first();
-  filters.hide();
 
-  let request = inviaRichiesta("get", "api/getCollections");
-  request.fail(errore);
-  request.done((collections) => {
-    console.log(collections);
+  filter.hide();
+
+  let request = inviaRichiesta("get", "/api/getCollections");
+  request.fail(errore)
+  request.done(function(collections) {
+    //console.log(collections);
     let label = divCollections.children("label");
-    for (const item of collections) {
+    for (const collection of collections) {
       let clone = label.clone();
-      divCollections.append(clone);
-      clone.children("input").val(item.name);
-      clone.children("span").text(item.name);
+      clone.appendTo(divCollections);
+      clone.children("input").val(collection.name);
+      clone.children("span").text(collection.name);
       divCollections.append("<br>");
     }
     label.remove();
-  });
+  })
 
-  divCollections.on("click", "input[type=radio]", function () {
+  divCollections.on("click","input[type=radio]",function(){
     currentCollection = $(this).val();
-    if (currentCollection === "unicorns") filters.show();
-    else filters.hide();
-    table.children("tbody").empty();
-    let request = inviaRichiesta("GET", `/api/${currentCollection}`);
+    let request = inviaRichiesta("GET", "/api/"+currentCollection)
     request.fail(errore);
-    request.done(disegnaTabella);
-    console.log();
+    request.done(disegnaTabella)
   });
-
-  function disegnaTabella(data) {
-    {
+  
+  function disegnaTabella(data){
       divIntestazione.find("strong").eq(0).text(currentCollection);
       divIntestazione.find("strong").eq(1).text(data.length);
+      if(currentCollection == "unicorns"){
+        filter.show();
+      }
+      else
+      {
+        filter.hide();
+      }
+      table.children("tbody").empty();
       for (const item of data) {
         let tr = $("<tr>");
         tr.appendTo(table.children("tbody"));
+
         let td = $("<td>");
-        tr.append(td.text(item._id));
-        td.prop({ id: item._id, method: "get" });
-        td.on("click", visualizzaDettagli);
+        td.appendTo(tr);
+        td.text(item._id);
+        td.prop("_id",item._id);
+        td.prop("method","get");
+        td.on("click", visualizzaDettagli)
+        
         td = $("<td>");
-        tr.append(td.text(item.name));
-        td.prop({ id: item._id, method: "get" });
-        td.on("click", visualizzaDettagli);
+        td.appendTo(tr);
+        td.text(item.name);
+        td.prop("_id",item._id);
+        td.prop("method","get");
+        td.on("click", visualizzaDettagli)
+
         td = $("<td>");
-        td.append(
-          $("<div>")
-            .prop({ id: item._id, method: "patch" })
-            .on("click", visualizzaDettagli)
-        );
-        td.append(
-          $("<div>")
-            .prop({ id: item._id, method: "put" })
-            .on("click", visualizzaDettagli)
-        );
-        td.append($("<div>").prop("id", item._id).on("click", elimina));
-        tr.append(td);
+        td.appendTo(tr);
+
+        $("<div>").appendTo(td).prop({"_id":item._id , "method":"patch"}).on("click",visualizzaDettagli); 
+        
+        $("<div>").appendTo(td).prop({"_id":item._id , "method":"put"}).on("click",visualizzaDettagli); 
+        
+        $("<div>").appendTo(td).prop("_id",item._id).on("click",elimina);
       }
-    }
-  }
+    };
 
-  function elimina() {
-    let request = inviaRichiesta(
-      "DELETE",
-      `/api/${currentCollection}/${$(this).prop("id")}`
-    );
+  function elimina(){
+    let request = inviaRichiesta("delete","/api/"+currentCollection +"/"+$(this).prop("_id"));
     request.fail(errore);
-    request.done((data) => {
-      aggiornaTabella();
-      alert("Documento eliminato correttamente");
-    });
+    request.done(function(){
+      alert("documento rimosso correttamente");
+      aggiorna();
+    })
+
   }
 
-  function visualizzaDettagli() {
-    const method = $(this).prop("method").toUpperCase();
-    const id = $(this).prop("id");
-    let req = inviaRichiesta("GET", `/api/${currentCollection}/${id}`);
-    req.fail(errore);
-    req.done((data) => {
-      if (method === "GET") {
+  function visualizzaDettagli(){
+    let method = $(this).prop("method").toUpperCase();
+    let id = $(this).prop("_id");
+    let request = inviaRichiesta("GET","/api/"+currentCollection +"/"+id)
+    request.fail(errore);
+    request.done(function(data){
+      console.log(data);
+      if(method == "GET"){
         let content = "";
         for (const key in data) {
-          content += `<strong>${key}</strong> ${data[key]} <br>`;
+          content += "<strong>"+ key +"</strong> : " + data[key] + "<br>";
+          divDettagli.html(content);
         }
-        divDettagli.html(content);
-      } else {
+      }
+      else{
         divDettagli.empty();
-        let txtarea = $("<textarea>");
-        delete data._id;
-        txtarea.val(JSON.stringify(data, null, 2));
-        txtarea.appendTo(divDettagli);
-        txtarea.css({ height: txtarea.get(0).scrollHeight + "px" });
-        creaPulsanteInvia(method, id);
+        let txtArea = $("<textarea>");
+        //rimuoviamo id perché non deve essere cambiato 
+        delete(data._id);
+        txtArea.val(JSON.stringify(data, null, 2));
+        txtArea.appendTo(divDettagli);
+        //ridimensionamento della textarea in base al contenuto
+        //ScrollHeight è una property js che restituisce l'altezza della textarea
+        //  sulla base del contenuto
+        txtArea.css("height",txtArea.get(0).scrollHeight);
+        console.log(txtArea.get(0).scrollHeight);
+        visualizzaBtnInvia(method, id);
       }
-    });
+    })
   }
 
-  $("#btnAdd").on("click", () => {
-    divDettagli.empty();
-    let txtarea = $("<textarea>");
-    txtarea.val("{}");
-    txtarea.appendTo(divDettagli);
-    creaPulsanteInvia("POST");
-  });
-
-  function aggiornaTabella() {
-    let event = jQuery.Event("click");
-    event.target = divCollections.find("input[type=radio]:checked")[0];
-    divDettagli.empty();
-    divCollections.trigger(event);
-  }
-
-  function creaPulsanteInvia(method, id = "") {
-    let button = $("<button>");
-    button.text("Invia");
-    button.appendTo(divDettagli);
-    button.on("click", () => {
-      let param;
-      try {
-        param = JSON.parse($(divDettagli).children("textarea").val());
-      } catch (error) {
-        alert("JSON NON VALIDO");
+  function visualizzaBtnInvia(method, id=""){
+    let btnInvia = $("<button>");
+    btnInvia.text("INVIA");
+    btnInvia.appendTo(divDettagli);
+    btnInvia.on("click", function(){
+      let param = "";
+      try{
+        param = JSON.parse(divDettagli.children("textarea").val());
       }
-      const request = inviaRichiesta(
-        method,
-        `/api/${currentCollection}/${id}`,
-        param
-      );
+      catch{
+        alert("Errore: Json non valido");
+        return;
+      }
+
+      let request = inviaRichiesta(method, "/api/"+currentCollection+"/"+id, param);
       request.fail(errore);
-      request.done((data) => {
-        divDettagli.empty();
-        aggiornaTabella();
-        alert("Operazione eseguita correttamente");
+      request.done(function(){
+        alert("operazione eseguita correttamente");
+        divDettagli.empty();    
+        aggiorna();
       });
-    });
+    })
   }
 
-  $("#btnFind").on("click", function () {
-    let filterJson = {};
-    let hair = $("#lstHair").children("option:selected").val();
-    if (hair) filterJson["hair"] = hair.toLowerCase();
-
-    let male = filter.find("input[type=checkbox]").first().is(":checked");
-    let female = filter.find("input[type=checkbox]").last().is(":checked");
-    if (male && !female) filterJson["gender"] = "m";
-    else if (female && !male) filterJson["gender"] = "f";
-
-    let request = inviaRichiesta("get", "/api/" + currentCollection, filterJson);
-    request.fail(errore);
-    request.done(disegnaTabella);
+  $("#btnAdd").on("click", function(){
+    divDettagli.empty();
+    let txtArea = $("<textarea>");
+    txtArea.val("{ }");
+    txtArea.appendTo(divDettagli);
+    visualizzaBtnInvia("POST");
   });
+
+  function aggiorna(){
+    //divCollections.trigger("click","input[type=radio]");
+    var event = jQuery.Event('click');
+    event.target = divCollections.find('input[type=radio]:checked')[0];
+    divCollections.trigger(event);
+    divDettagli.empty();
+  }
+
+  $("#btnFind").on("click", function(){
+		let filterJson = {}
+		let hair = $("#lstHair").children("option:selected").val()
+		if (hair)
+      filterJson["hair"]=hair.toLowerCase();
+		
+		let male = filter.find("input[type=checkbox]").first()
+				.is(":checked")
+		let female = filter.find("input[type=checkbox]").last()
+				.is(":checked")
+		if(male && !female)
+      filterJson["gender"]='m';
+		else if(female && !male)
+      filterJson["gender"]='f';
+		
+		let request = inviaRichiesta("get", "/api/" + currentCollection, filterJson)
+		request.fail(errore)
+		request.done(disegnaTabella)
+
+	})
 });
+
