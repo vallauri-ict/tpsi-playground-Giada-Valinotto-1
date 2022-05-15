4dc79abc6727c949ebcf377207786ac30a651391
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2022-05-15 14:11:05 +0200

    Getting started with Ionic framework

---
+++ Ionic/photo-gallery/src/app/tab2/tab2.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page implements OnInit {
+
+  constructor(private photoService:PhotoService) {}
+
+  ngOnInit(){
+    this.photoService.loadSaved();
+  }
+
+  addPhotoToGallery=()=>{
+    this.photoService.addNewToGallery();
+  }
+
+}
